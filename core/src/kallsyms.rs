70ce5081140a6188ff70407783aeec34dc512cb9
djnakum6239 <d.j.nakum6239@gmail.com>
2026-09-23 14:20:22 +0530

    test: fix kallsyms token fixture size

--- core/src/kallsyms.rs
+++ core/src/kallsyms.rs
@@ -311,7 +311,7 @@ mod tests {
         kernel[8..16].copy_from_slice(&0x2000u64.to_le_bytes());
         kernel[16..20].copy_from_slice(&2u32.to_le_bytes());
         kernel[20..24].copy_from_slice(&[1, 2, 3, 0]);
-        kernel[24..40].copy_from_slice(b"A\0B\0C\0D\0E\0F\0G\0H\0I\0J\0K\0L\0M\0N\0O\0P\0");
+        kernel[24..40].copy_from_slice(b"A\0B\0C\0D\0E\0F\0G\0H\0");
         for index in 0..256usize {
             let value = (index.min(15) * 2) as u16;
             let offset = 40 + index * 2;
